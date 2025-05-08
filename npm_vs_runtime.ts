6101d88967052260a60d18abcb5295261b722ce7
Chris Smallwood <94147451+chris-smallwood@users.noreply.github.com>
2025-05-08 12:38:22 -0600

    Create npm_vs_runtime.ts

---
+++ npm_vs_runtime.ts
@@ -0,0 +1,5 @@
+
+
+###test-db.ts in programtracker
+- Using a node package that is designed to be used with multiple runtimes e.g. node, bun, deno. etc.
+- This is why we need to pass node built in modules as args so the function can also take other runtime modules as args.
